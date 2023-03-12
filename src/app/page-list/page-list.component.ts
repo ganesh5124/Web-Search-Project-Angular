84c13030787ba5c3d50989b4628e86532d094171
Pepakayala Veera Ganesh Kumar <pepakayalaveeraganeshkumar@Pepakayalas-MacBook-Air.local>
2023-03-12 13:12:46 +0530

    First Steps of project

---
+++ src/app/page-list/page-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pageList',
+  templateUrl: './page-list.component.html',
+  styleUrls: ['./page-list.component.css']
+})
+export class PageListComponent {
+
+}
